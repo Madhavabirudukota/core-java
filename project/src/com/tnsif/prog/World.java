9b769990a145ba3765cc1f1795c7e657e4f4c4bb
DELL <DELL@10.0.0.115>
2023-09-20 10:05:30 +0530

    second commit

---
+++ project/src/com/tnsif/prog/World.java
@@ -0,0 +1,11 @@
+package com.tnsif.prog;
+
+public class World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello madhava");
+
+	}
+
+}
